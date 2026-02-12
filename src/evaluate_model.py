c77fee28ab0807c821c66dd9ef14022674107e91
cuolm <cuolm@protonmail.com>
2026-02-12 08:10:35 +0000

    feat: add configurable checkpoint evaluation and config dataclass
    Create Config dataclass for better data organization and --checkpoint arg to evaluate any training
    checkpoint.

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,27 +1,61 @@
 import os
 import json
+import gc
+import argparse
+from dataclasses import dataclass, field 
 from typing import Tuple, Dict
 import torch
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
-from datasets import load_dataset
+from transformers.trainer_utils import get_last_checkpoint
+from datasets import Features, Sequence, Value, IterableDataset, load_dataset
 from torch.utils.data import Dataset
 from pathlib import Path
 import matplotlib.pyplot as plt
 
+from finetune_model import FIMDataCollator
+from peft import PeftModel
 
-def check_device() -> str:
-    device = "cpu"
-    if torch.cuda.is_available():
-        device = "cuda"
-    elif torch.backends.mps.is_available():
-        device = "mps"
-    else:
-        device = "cpu"
-    return device
-    
+@dataclass
+class Config:
+    model_name: str = "Qwen/Qwen2.5-Coder-7B"
+    fim_pad_token: str = "<|fim_pad|>"
+    test_dataset_path: Path = field(init=False)
+    project_root_path: Path = field(init=False)
+    lora_adapter_path: Path = field(init=False)
+    lora_model_path: Path = field(init=False)
+    trainer_output_dir_path: Path = field(init=False) 
+    test_output_dir_path: Path = field(init=False)
 
-def evaluate_model(project_root_path: str, model: AutoModelForCausalLM, test_dataset: Dataset) -> Tuple[float, Dict] :
+    trainer_per_device_eval_batch_size: int = 4
+    device: str = field(init=False)
+
+    def __post_init__(self):
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        elif torch.backends.mps.is_available():
+            self.device = "mps"
+        else:
+            self.device = "cpu"
+
+        self.project_root_path = Path(__file__).resolve().parent.parent 
+        self.test_dataset_path = self.project_root_path / "datasets" / "test_dataset.jsonl"
+        self.lora_adapter_path = self.project_root_path / "lora_adapter"
+        self.lora_model_path = self.project_root_path / "lora_model"
+        self.trainer_output_dir_path = self.project_root_path / "results"
+        self.test_output_dir_path = self.project_root_path / "test_results"
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Comparison checkpoint")
+    parser.add_argument("--checkpoint",
+                        type=str, 
+                        required=True,
+                        metavar=""NAME_OR_LAST"",
+                        help='Checkpoint name, or "last"')
+    user_args = parser.parse_args()
+    return user_args
+
+def _evaluate_model(config: Config, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, test_dataset: Dataset) -> Tuple[float, Dict] :
     """
     Evaluates the model on the test dataset using Masked Cross-Entropy Loss.
 
@@ -37,70 +71,93 @@ def evaluate_model(project_root_path: str, model: AutoModelForCausalLM, test_dat
     If PP=50, the model is 5 times more uncertain. Lower PP is always better.
     """
     eval_args = TrainingArguments(
-        output_dir=f"{project_root_path}/eval_results", 
-        per_device_eval_batch_size=5, 
+        per_device_eval_batch_size=config.trainer_per_device_eval_batch_size,
+        fp16=True,  # Enables 16bit precision
+    )
+
+    data_collator = FIMDataCollator(
+        tokenizer=tokenizer,
+        label_pad_token_id=-100
     )
     
     trainer = Trainer(
-        model = model,
-        args = eval_args,
-        eval_dataset= test_dataset
+        model=model,
+        args=eval_args,
+        eval_dataset=test_dataset,
+        processing_class = tokenizer,
+        data_collator = data_collator
     )
 
     results = trainer.evaluate()
     perplexity = np.exp(results["eval_loss"])
+
+    if 'trainer' in locals():
+        del trainer  # Clear trainer references
+
     return perplexity, results
 
-def plot_loss(logging_steps: int, project_root_path: str) -> None:
-    losses_path = os.path.join(f"{project_root_path}/results", "training_log.json")
-    with open(losses_path, "r") as f:
-        data = json.load(f)
 
-    train_losses = data["train_losses"]
-    eval_losses = data["eval_losses"]
-    train_x = [i * logging_steps for i in range(len(train_losses))]
-    eval_x = [i * logging_steps for i in range(len(eval_losses))]
-    plt.figure(figsize=(8,5))
-    plt.plot(train_x, train_losses, label="Train Loss")
-    plt.plot(eval_x, eval_losses, label="Eval Loss")
-    plt.xlabel("Step")
-    plt.ylabel("Loss")
-    plt.title("Training and Evaluation Loss")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-
-def empty_device_cache(device: str) -> None:
-    if device == "cuda":
+def _clear_hardware_cache(config: Config) -> None:
+    gc.collect()
+    if config.device == "cuda":
+        torch.cuda.synchronize()
         torch.cuda.empty_cache()
-    elif device == "mps":
+    elif config.device == "mps":
         torch.mps.empty_cache()
-    # cpu: nothing to do
-    
+
 
 def main():
-    project_root_path = Path(__file__).resolve().parent.parent
-    device = check_device()
-    test_dataset = load_dataset("json", data_files=f"{project_root_path}/datasets/test_dataset.jsonl")["train"]
+    config = Config()
+    user_args = _parse_args()
 
-    #plot_loss(20, project_root_path)
+    dataset_features = Features({
+        'input_ids': Sequence(feature=Value(dtype='int32')),
+        'attention_mask': Sequence(feature=Value(dtype='int32')),
+        'labels': Sequence(feature=Value(dtype='int32')),
+    })
 
-    base_model_name = "Qwen/Qwen2.5-Coder-1.5B"
-    base_model = AutoModelForCausalLM.from_pretrained(base_model_name).to(device)
-    base_model_perplexity, base_model_results = evaluate_model(project_root_path, base_model, test_dataset)
+    test_dataset = load_dataset("json", data_files=str(config.test_dataset_path), features=dataset_features, streaming=True)["train"]
+
+    tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+    tokenizer.pad_token = config.fim_pad_token
+    tokenizer.padding_side = "right"
+
+    base_model = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=config.model_name,
+        dtype=torch.float16,
+        device_map="auto",
+    )
+    base_model_perplexity, base_model_results = _evaluate_model(config, base_model, tokenizer, test_dataset)
     print(base_model_perplexity)
     print(f"Base model perplexity: {base_model_perplexity}")
     print(f"Base model results: {base_model_results}")
 
     del base_model
-    empty_device_cache(device)
+    _clear_hardware_cache(config)
 
-    lora_model_name = f"{project_root_path}/lora_model"
-    lora_model = AutoModelForCausalLM.from_pretrained(lora_model_name).to(device)
-    lora_model_perplexity, lora_model_results = evaluate_model(project_root_path, lora_model, test_dataset)
+    if user_args.checkpoint == "last":
+        checkpoint_path = get_last_checkpoint(str(config.trainer_output_dir_path))
+    elif user_args.checkpoint:
+        checkpoint_path = str(config.trainer_output_dir_path / user_args.checkpoint)
+    else:
+        raise ValueError("No checkpoint specified. Use --checkpoint <name> or 'last'")
+
+    base_model_for_lora = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=config.model_name,
+        dtype=torch.float16,
+        device_map="auto"
+    )
+
+    lora_model = PeftModel.from_pretrained(
+        model = base_model_for_lora, 
+        model_id = checkpoint_path 
+    )
+    lora_model_perplexity, lora_model_results = _evaluate_model(config, lora_model, tokenizer, test_dataset)
     print(f"Lora finetuned model perplexity: {lora_model_perplexity}")
     print(f"Lora finetuned model results: {lora_model_results}")
+
+    del lora_model
+    _clear_hardware_cache(config)
 
 
 if __name__ == "__main__":
